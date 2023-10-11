1006d629729407804571a4efd2c2af4531eb4447
ghaidafasya24 <ghaidafasya5@gmail.com>
2023-10-11 20:11:59 +0700

    Pertemuan 3

---
+++ Pertemuan03/Praktikum/P3_2_714220031/P3_2_714220031/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_2_714220031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan Nilai N :");
+            int N = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("ANAK AYAM TURUN " + N);
+
+            if (N <= 0 || N > 10)
+            {
+                Console.WriteLine("Nilai tidak boleh dibawah 0 atau diatas 10");
+            }
+            else
+            {
+                for (int i = N; i >= 1; i--)
+                {
+                    if (i == 1)
+                    {
+                        Console.WriteLine("Anak ayam turun 1, mati satu tinggallah induknya");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Anak ayam turunlah {i}, mati satu tinggallah {i - 1}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Pertemuan03/Praktikum/P3_3_714220031/P3_3_714220031/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_3_714220031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Menu Pilihan:");
+                Console.WriteLine("1. Hitung Luas Persegi Panjang");
+                Console.WriteLine("2. Hitung Keliling Persegi Panjang");
+
+                Console.Write("Menu Pilihan : ");
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    // Menghitung Luas Persegi Panjang
+                    Console.Write("Masukkan panjang: ");
+                    double panjang = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Masukkan lebar: ");
+                    double lebar = Convert.ToDouble(Console.ReadLine());
+
+                    double luas = panjang * lebar;
+                    Console.WriteLine($"Luas Persegi Panjang: {luas}");
+                }
+
+                else if (input == "2")
+                {
+                    // Menghitung Keliling Persegi Panjang
+                    Console.Write("Masukkan panjang: ");
+                    double panjang = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Masukkan lebar: ");
+                    double lebar = Convert.ToDouble(Console.ReadLine());
+
+                    double keliling = 2 * (panjang + lebar);
+                    Console.WriteLine($"Keliling Persegi Panjang: {keliling}");
+                }
+                else
+                {
+                    Console.WriteLine("Menu tidak tersedia");
+                }
+
+                Console.Write("\nIngin mengulang kembali (Y/T)? ");
+            }
+
+            while (Console.ReadLine() == "Y");
+        }
+    }
+}
