4af92121931ea7f6f6c134f269233ed2089e8e4c
ghaidafasya24 <ghaidafasya5@gmail.com>
2023-10-18 19:23:54 +0700

    Pertemuan 4

---
+++ Pertemuan04/Praktikum/P4_1_714220031/P4_1_714220031/Book_714220031.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220031
+{
+    public class Book_714220031: Product_714220031
+    {
+        protected string pageCount;
+
+        public Book_714220031(string type, string title, string pagecount) : base(type, title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+            set
+            {
+                pageCount = value;
+            }
+        }
+    }
+}
---
+++ Pertemuan04/Praktikum/P4_1_714220031/P4_1_714220031/Product_714220031.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220031
+{
+    public class Product_714220031
+    {
+        private string myType;
+        private string myTitle;
+
+        public Product_714220031()
+        {
+           
+        }
+         public Product_714220031(string type, string title)
+        {
+            this.myType = type;
+            this.myTitle = title;
+        }
+    public string MyType
+    {
+        get
+        {
+            return myType;
+        }
+
+        set
+        {
+            myType = value;
+        }
+    }
+
+    public string MyTitle
+    {
+        get
+        {
+            return myTitle;
+        }
+
+        set
+        {
+            myTitle = value;
+        }
+    }
+    }
+    
+}
